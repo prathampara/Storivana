a264755d8f470ecf468adf917896859775a5c4a2
Pratham Parashar <parasharpratham2001@gmail.com>
2025-02-13 11:29:13 +0530

    git push origin main

--- src/main/java/com/blog/repository/PostRepo.java
+++ src/main/java/com/blog/repository/PostRepo.java
@@ -14,5 +14,5 @@ public interface PostRepo extends JpaRepository<Post, Integer> {
   List<Post> findByCategory(Category category);
   @Query("select p from Post p where p.title like:key")
   List<Post> searchByTitle(@Param("key")String title);
-
+//
 }
